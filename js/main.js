7bf4453123b03fef3cfaacfe4c01e1e80ef1a060
boomgreen87 <boomgreen87@yahoo.com>
2019-04-14 05:52:29 -0400

    Added mobile menu with functionality

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 (() => {
-    // STICKY MENU RULES
+    // BACK TO TOP RULES
 
     // Menu button variables
     var toTop = document.querySelector("#topBtn"),
@@ -14,4 +14,24 @@
     // Back to top on user click
     toTop.addEventListener("click", backToTop);
     desktopToTop.addEventListener("click", backToTop);
+
+
+
+    // MOBILE MENU RULES
+    var menu = document.querySelector("#mainNav"),
+        line1 = document.querySelector("#line1"),
+        line2 = document.querySelector("#line2"),
+        line3 = document.querySelector("#line3"),
+        menuButton = document.querySelector("#menuButton");
+
+    // Toggle menu function
+    function toggleMenu() {
+        menu.classList.toggle("openMenu");
+        line1.classList.toggle("openMenu");
+        line2.classList.toggle("openMenu");
+        line3.classList.toggle("openMenu");
+    }
+
+    // Toggle menu event listener, listens for user click
+    menuButton.addEventListener("click", toggleMenu);
 })();
