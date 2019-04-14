7217a497947450b79de108e9af56f6ec93682600
boomgreen87 <boomgreen87@yahoo.com>
2019-04-14 02:35:54 -0400

    Added sticky back to top button to desktop and fixed show statistics transition

--- js/main.js
+++ js/main.js
@@ -2,36 +2,16 @@
     // STICKY MENU RULES
 
     // Menu button variables
-    var menu = document.querySelector("#menu"),
-        stickyNav = document.querySelector("#stickyNav"),
-        line1 = document.querySelector("#line1"),
-        line2 = document.querySelector("#line2"),
-        line3 = document.querySelector("#line3"),
-        menuButton = document.querySelector("#menuButton"),
-        toTop = document.querySelector("#topBtn");
-
-    // Toggle menu function
-    function toggleMenu() {
-        console.log(line1);
-        console.log(line2);
-        console.log(line3);
-        console.log(menu);
-        stickyNav.classList.toggle("openMenu")
-        menu.classList.toggle("openMenu");
-        line1.classList.toggle("openMenu");
-        line2.classList.toggle("openMenu");
-        line3.classList.toggle("openMenu");
-    }
+    var toTop = document.querySelector("#topBtn"),
+        desktopToTop = document.querySelector("#desktopBackToTop");
 
     // Back to top function
     function backToTop(e) {
         e.preventDefault();
-        TweenLite.to(window, 1, {scrollTo:0});
+        TweenMax.to(window, 1, {scrollTo:0});
     }
-
-    // Toggle menu event listener, listens for user click
-    // menuButton.addEventListener("click", toggleMenu);
 
     // Back to top on user click
     toTop.addEventListener("click", backToTop);
+    desktopToTop.addEventListener("click", backToTop);
 })();
